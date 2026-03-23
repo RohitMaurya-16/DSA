9d3862f87cc24dd0dd0b6cb37fd5ca5d4a262bf7
Rohit Maurya <quickhelp36@gmail.com>
2026-03-23 11:12:39 +0530

    Time: 0 ms (100%), Space: 42.2 MB (28.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,26 @@
 class Solution {
     public int climbStairs(int n) {
-        if (n <= 2) return n;
+        // if (n <= 2) return n;
 
-        int first = 1;  // Ways to reach step 1
-        int second = 2; // Ways to reach step 2
+        // int first = 1;  // Ways to reach step 1
+        // int second = 2; // Ways to reach step 2
 
-        for (int i = 3; i <= n; i++) {
-            int current = first + second;
-            first = second; // Move 'first' pointer forward
-            second = current; // Move 'second' pointer forward
+        // for (int i = 3; i <= n; i++) {
+        //     int current = first + second;
+        //     first = second; // Move 'first' pointer forward
+        //     second = current; // Move 'second' pointer forward
+        // }
+
+        // return second;
+
+        int arr[]=new int[n+1];
+        arr[0]=1;
+        arr[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+         arr[i]=arr[i-1]+arr[i-2];
+
         }
-
-        return second;
+        return arr[n];
     }
 }
