121051911678043d497bb2c77e043f0758568692
Rohit Maurya <quickhelp36@gmail.com>
2026-03-28 19:48:08 +0530

    Time: 2 ms (99.27%), Space: 47.3 MB (32.69%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,4 +1,6 @@
-import java.util.*;
+// import java.util.*;
+
+// Approach 1
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
@@ -19,3 +21,82 @@ class Solution {
         return new int[]{};
     }
 }
+
+
+// Approach 2
+
+// import java.util.*;
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int n = nums.length;
+//         for(int i = 0; i<n-1;i++){
+//             for(int j =i+1; j<n; j++){
+//                 if(nums[i]+nums[j]==target){
+//                     return new int[] {i,j};
+//                 }
+//             }
+//         }
+//         return new int[]{};
+//     }
+// }
+
+
+// Approach 3
+
+// public class Solution
+// {
+//     public static int[] twoSum(int arr[],int k)
+//     {
+//        int left=0;
+//        int right=(arr.length-1);
+//        while(left<right)
+//         {  
+//             if(arr[left]+arr[right]==k)
+//                 {
+//                     return new int[] {left,right};
+//                 }
+//              else
+//              {
+//                  right--;
+//              }
+//              if(right==left)
+//              {
+//                  right=arr.length-1;
+//                  left++;
+//              }
+//             }
+//             return new int[] { };
+//         }
+// }
+
+
+// Brute Force
+
+// import java.util.*;
+// public class TwoSum
+// {
+//     public static int[] twosum(int arr[],int k)
+//     {
+//         for(int i=0;i<arr.length;i++)
+//         {
+//             for(int j=i;j<arr.length;j++)
+//             {
+//                 if(arr[i]+arr[j]==k)
+//                 {
+//                     return new int[]{i,j};
+//                 }
+//             }
+//         }
+//         return new int[]{ };
+//     }
+    
+//     public static void main(String args[])
+//     {
+//         Scanner sc=new Scanner(System.in);
+        
+//         int arr[]={1,2,5,7,9,3};
+//         int k=16;
+//         System.out.println(Arrays.toString(TwoSum.twosum(arr,k)));
+//     }
+// }
