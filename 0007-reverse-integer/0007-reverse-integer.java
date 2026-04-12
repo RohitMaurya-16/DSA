a5ff5ed6acc06685535e35e7f609a1fcbadfe205
Rohit Maurya <quickhelp36@gmail.com>
2026-04-12 17:39:12 +0530

    Time: 2 ms (8.51%), Space: 42.9 MB (17.04%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,6 +1,35 @@
 class Solution {
     public int reverse(long x) {
      
+    /*    
+
+    Before going to code you should know;
+
+    String s = Long.toString(val);   // Converts long val into String s;
+    char[] ch = s.toCharArray();    // Conver string to array
+
+    exmaple:
+
+    val=1234---> s="1234"---> ch=[1,2,3,4]
+
+    String s1= new String(ch);   // s1="4321"  --> Value after performing a operation.
+
+    long num=Long.parseLong(s1);  // num=4321
+
+    Note: 1. Handle negative carefully.
+          2. If range of input not inint range return 0;
+          3. Check 2nd step by using this code-->
+
+          if (num > Integer.MAX_VALUE || num < Integer.MIN_VALUE) {
+    return 0;
+}
+
+If you know these concepts this question will be cakewalk for you..
+
+    */
+
+
+
      boolean neg=x<0;    // -123   true
      
      long val=Math.abs(x);
@@ -31,3 +60,4 @@ class Solution {
       return(int) num;
     }
 }
+
