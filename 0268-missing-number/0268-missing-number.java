5c9b79410eb3a8366fc544d62a86bfb5d6476105
Rohit Maurya <quickhelp36@gmail.com>
2026-04-24 22:12:15 +0530

    Time: 0 ms (100%), Space: 46.9 MB (95.51%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,20 +1,36 @@
 class Solution {
     public int missingNumber(int[] nums) {
         
-        Arrays.sort(nums);
-        int a=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            int k=nums[i];
-            if(k==a)
-            {
-             a++;
-            }
-            else
-            {
-                return a;
-            }
+//         Arrays.sort(nums);
+//         int a=0;
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             int k=nums[i];
+//             if(k==a)
+//             {
+//              a++;
+//             }
+//             else
+//             {
+//                 return a;
+//             }
      
-       }  
-    return nums.length; }
+//        }  
+//     return nums.length; }
+// }
+
+// The above code is not optimal so lets do optimal code
+
+
+   int n=nums.length;
+   int sum=n*(n+1)/2;
+   int acc=0;
+
+   for(int i=0;i<n;i++)
+   {
+      acc+=nums[i];
+   }
+
+   return (sum-acc);
+    }
 }
