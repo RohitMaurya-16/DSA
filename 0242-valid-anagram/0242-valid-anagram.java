22f42490df39a8ea172359fb4eeab42002cd3c00
Rohit Maurya <quickhelp36@gmail.com>
2026-08-10 16:10:26 +0530

    Time: 8 ms (29.99%), Space: 43.8 MB (99.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-         
+         s = s.toLowerCase();
          if(s.length()!=t.length())return false;
          int count[]=new int[26];
          for(int i=0;i<s.length();i++)
