c5439eccaaa0455b2d5f4af2ab6a7f70fd3892bf
Rohit Maurya <quickhelp36@gmail.com>
2026-08-15 11:06:47 +0530

    Time: 0 ms (100%), Space: 42.8 MB (56.29%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.java
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.java
@@ -13,11 +13,11 @@ class Solution {
         
         int binary=0;
 
-       ListNode dummy= head;
+       ListNode dummy= head;   // use to declare new node dummy just like int here we use ListNode.
 
         while(dummy!=null)
         {
-            binary=binary*2+dummy.val;
+            binary=binary*2+dummy.val;  // If you need to conv. binary to int just traverse one by one and multiply by 2.
 
             dummy=dummy.next;
         }
