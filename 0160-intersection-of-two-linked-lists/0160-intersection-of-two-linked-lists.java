8efd27da8f455438c123e0149454ff1218609ddd
Rohit Maurya <quickhelp36@gmail.com>
2026-08-15 12:33:52 +0530

    Time: 1 ms (99.91%), Space: 52.9 MB (45.72%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -11,24 +11,81 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        HashSet<ListNode> list= new HashSet<>();
+//         HashSet<ListNode> list= new HashSet<>();
 
-        while(headA!=null)
-        {
-            list.add(headA);
-            headA=headA.next;
-        }
+//         while(headA!=null)
+//         {
+//             list.add(headA);
+//             headA=headA.next;
+//         }
 
-        while(headB!=null)
-        {
-            if(list.contains(headB))
-            {
-                return headB;
-            }
+//         while(headB!=null)
+//         {
+//             if(list.contains(headB))
+//             {
+//                 return headB;
+//             }
 
-            headB=headB.next;
-        }
+//             headB=headB.next;
+//         }
 
-        return null;
+//         return null;
+//     }
+// }
+
+ListNode nodeA=headA;
+ListNode nodeB=headB;
+
+int lenA=0;
+int lenB=0;
+
+while(nodeA!=null)
+{
+    lenA++;
+    nodeA=nodeA.next;
+}
+
+while(nodeB!=null)
+{
+    lenB++;
+    nodeB=nodeB.next;
+}
+
+nodeA=headA;
+nodeB=headB;
+
+if(lenA>lenB)
+{
+    int diff=lenA-lenB;
+    while(diff>0)
+    {
+        nodeA=nodeA.next;
+        diff--;
     }
 }
+else
+{
+    int diff=lenB-lenA;
+    while(diff>0)
+    {
+        nodeB=nodeB.next;
+        diff--;
+    }
+}
+
+ListNode a=nodeA;
+ListNode b=nodeB;
+
+while(a!=null && b!=null)
+{
+    if(a==b)return a;
+    a=a.next;
+    b=b.next;
+}
+
+return null;
+    }
+}
+
+
+
