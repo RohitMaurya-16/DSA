4a7439bdfee80d5c88f65d504c4b50f9bfc847a3
Rohit Maurya <quickhelp36@gmail.com>
2026-08-27 16:39:50 +0530

    Time: 816 ms (30.66%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+SELECT 
+    s.user_id, 
+    IFNULL(
+        ROUND(
+            COUNT(CASE WHEN c.action='confirmed' THEN 1 END) * 1.0 / 
+            NULLIF(COUNT(CASE WHEN c.action='confirmed' OR c.action='timeout' THEN 1 END), 0), 
+            2
+        ), 
+        0
+    ) AS confirmation_rate
+FROM Signups s 
+LEFT JOIN Confirmations c ON s.user_id = c.user_id
+GROUP BY s.user_id;
+
+# if data is null it will add as 0
