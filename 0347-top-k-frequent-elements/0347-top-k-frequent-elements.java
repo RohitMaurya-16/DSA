40963beacfdf8def4df063fb5c0a511e8dcd7895
Rohit Maurya <quickhelp36@gmail.com>
2026-09-19 21:43:35 +0530

    Time: 11 ms (96.11%), Space: 47.8 MB (20.85%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,29 +1,23 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        
-       // Initialize the dynamic list
-List<int[]> list = new ArrayList<>();
-Arrays.sort(nums); // Sort to group identical numbers
+        Map<Integer, Integer> map= new HashMap<>();
+        PriorityQueue<int[]> pq= new PriorityQueue<>((a,b)->b[1]-a[1]);
+        for(int x:nums)
+        {
+            map.merge(x,1,Integer::sum);
+        }
+      for(Map.Entry<Integer,Integer> entry: map.entrySet())
+      {
+        pq.offer(new int[]{entry.getKey(),entry.getValue()});
+      }
 
-for (int i = 0; i < nums.length; i++) {
-    int count = 1;
-    // Jump through duplicates
-    while (i < nums.length - 1 && nums[i] == nums[i + 1]) {
-        count++;
-        i++;
-    }
-    // Store pair: [the number, its count]
-    list.add(new int[]{nums[i], count});
-}
+      int arr[]= new int[k];
 
-list.sort((a,b)->b[1]-a[1]);
+      for(int i=0;i<k;i++)
+      {
+        arr[i]=pq.poll()[0];
+      }
 
-int[] x=new int[k];
-for(int i=0;i<k;i++)
-{
-x[i]=list.get(i)[0];
-}
-
-return x;
+      return arr;
     }
 }
